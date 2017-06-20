430d312c0b46569e418327557c4842e22ac32147
RQMEQ <roman_g86@mail.ru>
2017-06-20 09:39:10 +0300

    Готово

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
 	$('.menu-trigger').click(function() {
 		$('nav ul').slideToggle(500);
   });
+	$('.nav ul li').click(function() {
+		$('nav ul').slideToggle(500);
+  });
 
 	$(window).resize(function() {		
 		if (  $(window).width() > 500 ) {			
